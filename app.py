a18c7d6c62f916ada12eb6228a39dc58ac4aadc1
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2022-10-31 14:00:36 -0300

    Add some put and delete method and update docs

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from routes.user import user
+from docs import tags_metadata
 
 
-app = FastAPI()
+app = FastAPI(
+    title='REST API with FastAPI and Mongo DB',
+    description= 'This is a basic REST API using FastAPI and MongoDB',
+    version='1.0.0',
+    openapi_tags=tags_metadata
+)
 
 app.include_router(user)
--- routes/user.py
+++ routes/user.py
@@ -1,40 +1,45 @@
-from hashlib import new
-from operator import ne
-from re import U
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from config.db import conn
 from schemas.user import userEntity, usersEntity
 from models.user import User
 from passlib.hash import sha256_crypt
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
 
 user = APIRouter()
 
-@user.get('/users')
+
+@user.get('/users', response_model=list[User], tags=['users'])
 def find_all_user():
     return usersEntity(conn.local.user.find())
 
-@user.post('/users')
+
+@user.post('/users', response_model=User, tags=['users'])
 def create_user(user: User):
-    
+
     new_user = dict(user)
     new_user['password'] = sha256_crypt.encrypt(new_user['password'])
     del new_user['id']
-    
+
     id = conn.local.user.insert_one(new_user).inserted_id
 
     user_created = conn.local.user.find_one({'_id': id})
 
     return userEntity(user_created)
-    
 
-@user.get('/users/{id}')
-def find_user():
-    return 'Hello World'
 
-@user.put('/users/{id}')
-def update_user():
-    return 'Hello World'
+@user.get('/users/{id}', response_model=User, tags=['users'])
+def find_user(id: str):
+    return userEntity(conn.local.user.find_one({'_id': ObjectId(id)}))
 
-@user.delete('/users/{id}')
-def delete_user():
-    return 'Hello World'
+
+@user.put('/users/{id}', response_model=User, tags=['users'])
+def update_user(id: str, user: User):
+    conn.local.user.find_one_and_update({'_id': ObjectId(id)}, {'$set': dict(user)})
+    return userEntity(conn.local.user.find_one({'_id': ObjectId(id)}))
+
+
+@user.delete('/users/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['users'])
+def delete_user(id: str):
+    userEntity(conn.local.user.find_one_and_delete({'_id': ObjectId(id)}))
+    return Response(status_code=HTTP_204_NO_CONTENT)
